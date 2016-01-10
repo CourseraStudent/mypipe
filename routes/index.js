e441b47192aa259193708d9838a68b5b00fe8414
CourseraStudent <you@example.com>
2016-01-11 00:50:45 +0300

    main html page template

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var mypipefs = req.mypipefs;
-  res.render('index', { title: mypipefs.path });
+  res.render('index', { /*title: mypipefs.path*/ });
 });
 
 
