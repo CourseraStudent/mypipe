a9a0779fdafa3a9eccd07ffac58439c0ea9921c8
CourseraStudent <you@example.com>
2016-01-12 01:57:49 +0300

    + ChannelListComponent Click

--- public/javascripts/react-mypipe-components.js
+++ public/javascripts/react-mypipe-components.js
@@ -1,10 +1,16 @@
 
 var Channel = React.createClass({
+  onClickInternal: function() {
+    this.props.onClick(this.props.dataMember);
+  },
+
   render: function () {
+    var onClickInternal = this.onClickInternal;
     return (
-      <div>
+      <div onClick={onClickInternal}>
         <div>{this.props.icon}</div>
         <span>{this.props.name}</span>
+        <span>[{this.props.active}]</span>
       </div>
     );
   }
@@ -14,30 +20,56 @@ var Channel = React.createClass({
 var ChannelList = React.createClass({
   getInitialState: function() {
     return { 
-      'ActiveChannelIndex': 0,
+      'ActiveChannel': null
     }
   },
   getDefaultProps: function() {
     return {
-      'ChannelList': [ { name: 'loading...', icon: '' } ],
-      'OnChannelChanged': function(){}
+      'ChannelList': [],
+      'OnChannelChanged': function(){ console.log('click was not handled'); }
     };
   },
 
-  getActiveChannel: function() {
-    return this.props.ChannelList[this.state.ActiveChannelIndex];
-  },
   update: function(ChannelList) {
     this.setProps({'ChannelList': ChannelList});
-    this.props.onChannelChanged();
+    this.props.OnChannelChanged();
+  },
+  setActiveChannelSilently: function(channel) {
+    this.state.ActiveChannel = channel;
+  },
+  getActiveChannel: function() {
+    return this.state.ActiveChannel;
+  },
+  onChannelClicked: function(channel) {
+    this.setState({ 'ActiveChannel': channel});
+    this.props.OnChannelChanged(channel);
+
+  },
+  areChannelEqual: function(channel1, channel2) {
+    var eq = channel1 == channel2 || 
+        channel1.name == channel2.name;
+    return eq;
+  },
+  isChannelActive: function(channel) {
+    return this.areChannelEqual(this.state.ActiveChannel, channel) ?
+      'active' : '';
   },
 
   render: function() {
-    console.log('render', this.props)
+    if(!this.state.ActiveChannel && this.props.ChannelList.length > 0)
+      this.setActiveChannelSilently(this.props.ChannelList[0]);
+    
+    var onChannelClicked = this.onChannelClicked;
+    var that = this; // TODO find a better solution
     var list = this.props.ChannelList.map(function(channel) {
       return (
         <li>
-          <Channel name={channel.name} icon={channel.icon} />
+          <Channel 
+            name={channel.name} 
+            icon={channel.icon} 
+            active={that.isChannelActive(channel)}
+            onClick={that.onChannelClicked} 
+            dataMember={channel} />
         </li>
       )
     });
@@ -57,7 +89,7 @@ window.componentRenderHelper = (function() {
 
   function renderChannelList(containerID, onChannelChanged) {
     return ReactDOM.render(
-        <ChannelList onChannelChanged={onChannelChanged} />,
+        <ChannelList OnChannelChanged={onChannelChanged} />,
         getContainer(containerID)
       );
   };
