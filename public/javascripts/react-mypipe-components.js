82fc133b68f1a4dbbf7c27190dbc1c40187e8f59
CourseraStudent <you@example.com>
2016-01-11 23:10:06 +0300

    react.js - replace 'foreach' for 'map'

--- public/javascripts/react-mypipe-components.js
+++ public/javascripts/react-mypipe-components.js
@@ -1,36 +1,38 @@
 
 var Channel = React.createClass({
-	render: function () {
-		return (
-			<div>
-				<div>{this.props.icon}</div>
-				<span>{this.props.name}</span>
-			</div>
-		);
-	}
+  render: function () {
+    return (
+      <div>
+        <div>{this.props.icon}</div>
+        <span>{this.props.name}</span>
+      </div>
+    );
+  }
 });
 
 
 var ChannelList = React.createClass({
-	render: function() {
-		var channels = this.props.ChannelList;
-
-		var list = [];
-		channels.forEach(function(channel){
-			list.push(<li><Channel name={channel.name} icon={channel.icon} /></li>);
-		});
-
-		return (
-			<ul>
-				{list}
-			</ul>
-		);
-	}
+  render: function() {
+    var channels = this.props.ChannelList;
+      return (
+        <ul>
+          {
+            channels.map(function(channel) {
+              return (
+                <li>
+                  <Channel name={channel.name} icon={channel.icon} />
+                </li>
+              )
+            })
+          }
+        </ul>
+      );
+  }
 });
 
 window.createChannelList = function(container, channelData) {
-	ReactDOM.render(
-    	<ChannelList ChannelList={channelData} />,
-    	container
+  ReactDOM.render(
+      <ChannelList ChannelList={channelData} />,
+      container
     );
 };
